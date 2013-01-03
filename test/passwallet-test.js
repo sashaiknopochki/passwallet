6809c6266aa0cf27dc67fbf1f578286132704766
Simon Mayes <simon@msyea.co.uk>
2013-01-03 15:37:49 +0000

    Introduced middleware method

--- test/passwallet-test.js
+++ test/passwallet-test.js
@@ -1,15 +1,24 @@
 var express = require('express'),
 	passwallet = require('passwallet'),
-	request = require('supertest');
+	request = require('supertest'),
+	assert = require('assert');
 
 var app = express()
-		.use(passwallet());
+		.use(passwallet.middleware());
 
 var webServiceURL = '',
 	version = 1,
-	deviceLibraryIdentifier = '',
-	passTypeIdentifier = '',
-	serialNumber = '';
+	deviceLibraryIdentifier = 'x',
+	passTypeIdentifier = 'y',
+	serialNumber = 'z';
+
+var Pass = function() {
+
+};
+
+var passes = [
+
+];
 
 var registerRequest = {
 	url: webServiceURL+'/'+version+'/devices/'+deviceLibraryIdentifier+'/registrations_attido/'+passTypeIdentifier+'/'+serialNumber,
@@ -25,38 +34,66 @@ var registerRequest = {
 };
 
 describe('API', function() {
-	describe('registering device', function () {
+	describe('load', function () {
+		beforeEach(function() {
+			passwallet.Pass = undefined;
+		});
+		it('good pass model', function(done) {
+			var pass = {pushServiceURL: null};
+			passwallet.loadPass(pass);
+			assert.equal(passwallet.Pass, pass);
+			done();
+		});
+		it('bad pass model', function(done) {
+			var pass = {};
+			assert.throws(function () {
+				passwallet.loadPass(pass);
+			});
+			assert.equal(passwallet.Pass, undefined);
+			done();
+		});
+	});
+	describe('registering device', function() {
+		before(function() {
+			// this needs to be delayed because in MVC models aren't typically loaded until way after the middleware
+			passwallet.loadPass(Pass);
+		});
 		it('register device', function(done) {
 			request(app)
 				.post(registerRequest.url)
 				.set(registerRequest.headers)
 				.send(registerRequest.data)
-				.expect(202) // Created
-				.end(done);
+				.expect(202, done); // Created
 		});
 		it('register device again', function(done) {
 			request(app)
 				.post(registerRequest.url)
 				.set(registerRequest.headers)
 				.send(registerRequest.data)
-				.expect(200) // OK
-				.end(done);
+				.expect(200, done); // OK
 		});
 		it('bad authorisation', function(done) {
 			request(app)
 				.post(registerRequest.url)
-				.set(registerRequest.headers)
+				// .set(registerRequest.headers)
 				.send(registerRequest.data)
-				.expect(401) // Unauthorised
-				.end(done);
+				.expect(401, done); // Unauthorised
 		});
 		it('malformed request', function(done) {
 			request(app)
 				.post(registerRequest.url)
 				.set(registerRequest.headers)
 				.send(registerRequest.data)
-				.expect(400) // Bad Request
-				.end(done);
+				.expect(400, done); // Bad Request
+		});
+	});
+	describe('push update notification', function() {
+		it('single', function(done) {
+			passwallet.pushUpdate({}, function (err, passes) {
+				if (err) throw err;
+				assert.equal(null, passes);
+				done();
+			});
 		});
 	});
 });
